403ac0691a0dd681af990f427e58b8510f2eb50d
EugeneRuzanov <eugene.ruzanov@gmail.com>
2016-10-06 23:54:04 +0300

    add arrows

--- js/shopCatalog.mobile.js
+++ js/shopCatalog.mobile.js
@@ -2,17 +2,20 @@
   'use strict';
   let shopCatalogMobile = document.querySelector( '.shop-catalog-mobile' );
   function renderMenu( data ) {
-    let container = document.createElement( 'ul' ), index = 0, li;
+    let container = document.createElement( 'ul' ), index = 0, li, a;
     for ( ;index < data.length; index++ ) {
       li = document.createElement( 'li' );
-      li.textContent = data[index].name;
+      a = document.createElement( 'a' );
+      a.textContent = data[index].name;
+      li.appendChild( a );
       container.appendChild( li );
       if ( data[index].sub ) {
+        li.classList.add( 'shop-catalog-mobile__item_width-sub-menu' );
         container.appendChild( renderMenu( data[index].sub ) );
       }
     }
     return container;
   }
   shopCatalogMobile.appendChild( renderMenu( shopCatalog ) );
-  FastClick.attach( document.body );
+  //FastClick.attach( document.body );
 } )();
