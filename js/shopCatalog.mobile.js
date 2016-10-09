7b80ee3f2709873ec624172ed1209614d867f787
EugeneRuzanov <eugene.ruzanov@gmail.com>
2016-10-09 23:40:50 +0300

    перезагрузка контейнера каталога и заголовка

--- js/shopCatalog.mobile.js
+++ js/shopCatalog.mobile.js
@@ -11,22 +11,23 @@
   }
 
   function onClick ( e ) {
-    //todo перезагрузка контейнера каталога и заголовка
-    let title = e.target.closest( '.shop-catalog-mobile__item_width-sub-menu' ).textContent;
-    headerTitle.textContent = title;
+    let itemMenu = e.target.closest( '.shop-catalog-mobile__item_width-sub-menu' );
+    headerTitle.textContent = shopCatalog[itemMenu.dataset.path].name;
+    shopCatalogMobile.innerHTML = '';
+    shopCatalogMobile.appendChild( renderMenu( shopCatalog[itemMenu.dataset.path].sub, itemMenu.dataset.path ) );
   }
 
-  function renderMenu ( data ) {
+  function renderMenu ( data, path ) {
     let container = document.createElement( 'ul' ), index = 0, li, a;
     for ( ;index < data.length; index++ ) {
-      li = document.createElement( 'li' );
       a = document.createElement( 'a' );
       a.textContent = data[index].name;
+      li = document.createElement( 'li' );
+      li.dataset.path = path ? path + '-' + index : index;
       li.appendChild( a );
       if ( data[index].sub ) {
         li.classList.add( 'shop-catalog-mobile__item_width-sub-menu' );
         li.addEventListener( 'click', onClick );
-        //li.appendChild( renderMenu( data[index].sub ) );
       }
       container.appendChild( li );
     }
